35d95f17af738e50369f73eeb0c6351e96e32b54
YPermitin <ypermitin@yandex.ru>
2020-11-21 18:38:29 +0500

    Добавил вспомогательные функции работы со строками

---
+++ YY.TechJournalReaderAssistant/Helpers/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace YY.TechJournalReaderAssistant.Helpers
+{
+    internal static class StringExtensions
+    {
+        public static string CreateMD5(this string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
